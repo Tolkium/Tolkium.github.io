b4101aabe657526947201d258ec7145f1e65914e
Tolkium <tolkium@gmail.com>
2025-10-03 17:17:06 +0200

    feat(ui): Angular 20 upgrade, Heroicons menu, settings page with hide-scrollbar, mobile dark-mode transition, menu spacing tweaks

--- src/app/models/menu-data.ts
+++ src/app/models/menu-data.ts
@@ -5,14 +5,12 @@ export const MENU_SECTIONS: MenuSection[] = [
   {
     title: 'ACTIVITY DATA & TARGETS',
     items: [
-      { iconName: 'heroSquares2x2', label: 'Home', route: '/targets' },
+      { iconName: 'heroSquares2x2', label: 'Home', route: '/activities' },
       { iconName: 'heroFolderOpen', label: 'Projects', route: '/projects' },
       { iconName: 'heroPhoto', label: 'Gallery', route: '/hierarchy' },
       { iconName: 'heroPuzzlePiece', label: 'TODO list', route: '/departments' },
       { iconName: 'heroCalendar', label: 'Personal calendar', route: '/calendar' },
       { iconName: 'heroCube', label: 'Docker Dashboard', route: '/DockerDashboard' },      
-
-      
 
     ]
   },
@@ -28,15 +26,39 @@ export const MENU_SECTIONS: MenuSection[] = [
       { iconName: 'heroArchiveBox', label: 'Snippet Vault', route: '/SnippetVault' },
       { iconName: 'heroServer', label: 'Local Storage Viewer', route: '/SkillMatrix' },
       // { iconName: 'heroSparkles', label: 'Glitch Art Generator', route: '/SkillMatrix' },
-      // { iconName: 'heroBeaker', label: 'Code Pattern Library', route: '/CodePatternLibrary' },
+      // { iconName: 'heroBeaker', label: 'Code Pattern Library', route: '/CodePatternLibrary' }
+      { iconName: 'heroChartPie', label: 'Server Analitics', route: '/DockerDashboard' },
     ]
   },
   {
     title: 'USERS & ROLES',
     items: [
+
       { iconName: 'heroArrowDownTray', label: 'Download CV as PDF', route: '/roles' },
       { iconName: 'heroUserGroup', label: 'User Management', route: '/User' },
       { iconName: 'heroCog6Tooth', label: 'Setings', route: 'Setings' }
     ]
   } 
 ];
+
+
+
+// ACTIVITY DATA & TARGETS
+// Home
+// Projects
+// Gallery
+// TODO list
+// Personal calendar
+// Docker Dashboard
+// MASTERS
+// Regex Tester
+// Color Palette Generator
+// GitHub Repo Explorer
+// Package Version Checker
+// Skill Matrix
+// Snippet Vault
+// Local Storage Viewer
+// USERS & ROLES
+// Download CV as PDF
+// User Management
+// Setings
